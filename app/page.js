841e81480897db0f0eb2d665c81c68ecdd142a33
Phakjiraaaaaa <phakjira050349@gmail.com>
2025-06-18 10:40:44 +0700

    V.2

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
-   <h1>Miss Phakjira Pakphian</h1>
+   <h1><center>Miss Phakjira Pakphian</center></h1>
    </>
   );
 }
